67f2d61f149a51484931ee813e7f81e0860c5618
iEnis <62299034+EnisGamingX@users.noreply.github.com>
2023-11-19 12:40:32 +0100

    Delete Craft Ops I/behavior/scripts/index.js

--- Craft Ops I/behavior/scripts/index.js
+++
@@ -1,9 +0,0 @@
-import { system, world } from "@minecraft/server";
-system.beforeEvents.watchdogTerminate.subscribe((data) => {
-    data.cancel = true;
-    for (let player of world.getAllPlayers()) {
-        if (!player.isOp())
-            continue;
-        player.sendMessage(`Server tried to crash:\n${data.terminateReason}`);
-    }
-});
